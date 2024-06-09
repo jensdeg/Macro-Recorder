ca4d0dad4f9f9ec671c0f004996079a4a3111e8b
jensdeg <jensdegraaf88@gmail.com>
2024-06-09 15:19:01 +0200

    small cleanup

--- src/recorder.py
+++ src/recorder.py
@@ -2,26 +2,25 @@ import pyautogui
 import win32api
 import time
 
-#Recorder
 Recording = []
 
 
 def Start():
     print("Recording started...")
-    # defines time the recorder started
     
-    firstclick = True
     while True:
         # x,y are location. T is time it was clicked         
         X,Y,T = GetClick(time.perf_counter())
+
+        # adds click to recording
         click = (X,Y),T
         Recording.append(click)
+
         print(Recording)
-        # print('location: ({},{}) Clicked at {} seconds'.format(X,Y,T))
 
 
 
-
+# Gets the entire click event including position and time
 def GetClick(Starting_Time):
     X,Y = getPosition()
     T = GetTime(Starting_Time)
@@ -32,7 +31,7 @@ def GetClick(Starting_Time):
 def GetTime(Starttime):
     return time.perf_counter() - Starttime
 
-
+# gets current mouse position
 def getPosition():
     # from https://stackoverflow.com/questions/165495/detecting-mouse-clicks-in-windows-using-pythonimport pyautogui
     state_left = win32api.GetKeyState(0x02)  # right button down = 0 or 1. Button up = -127 or -128
