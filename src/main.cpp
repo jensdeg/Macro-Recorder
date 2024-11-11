454539fde070e814f3f969871a93bc8c31c8c739
jensdeg <jensdegraaf88@gmail.com>
2024-11-11 21:37:21 +0100

    Get mouse clicks

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+
+POINT mousepos;
+UINT button;
+
+bool mouseclicked = false;
+std::vector<POINT> clicks;
+
+
+bool MouseClickedEvent(){
+    if (GetAsyncKeyState(0x01)){
+        if(!mouseclicked){
+            mouseclicked = true;
+            return true;
+        }    
+    } else {
+        mouseclicked = false;
+    }
+    return false;
+}
+
 
 int main(){
-    std::cout << "hello world" << std::endl;
+
+    
+    //mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+    while (true)
+    {
+        //exits if deletes is pressed
+        if(GetAsyncKeyState(0x2E)){
+            for(int i = 0; i<clicks.size(); i++){
+                SetCursorPos(clicks[i].x, clicks[i].y);
+
+                std::cout << clicks[i].x << " : " << clicks[i].y << std::endl;
+            }
+            return EXIT_SUCCESS;
+        }
+
+        if(MouseClickedEvent()){
+            GetCursorPos(&mousepos);
+            clicks.push_back(mousepos);
+        }
+    }
     return EXIT_SUCCESS;
 }
+
+
+
+
